2ef1c97cfb22f72cbe06d296cbafb85ae1b774b2
sarvesh <sarveshhyadav343@gmail.com>
2026-02-09 15:07:42 +0530

    hello

--- basic programmes/02-Java-Primitive-Data-Type/02-Java-Primitive-Data-Type/AgeOfHarry.java
+++
@@ -1,8 +0,0 @@
-public class AgeOfHarry{
-	public static void main(String[] args) {
-	    String name = "Harry";
-	    int dob = 2000;
-	    int currentyear = 2024;
-		System.out.println("Harry's age in "+currentyear+" is "+(currentyear - dob));
-	}
-}
--- basic programmes/02-Java-Primitive-Data-Type/02-Java-Primitive-Data-Type/KmToMiles.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class KmToMiles{
-	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-	    int km = sc.nextInt();
-	    double miles = km * 1.6;
-		System.out.println("The total miles is "+miles+" mile for the given "+km+" km");
-	}
-}
